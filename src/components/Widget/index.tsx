7ef98f5bfa9b7221853f32945d1186461da6bca1
raisa-design <almeida.alves.raisa@gmail.com>
2022-06-14 15:25:12 -0300

    Mobile

---
+++ src/components/Widget/index.tsx
@@ -0,0 +1,13 @@
+import React from  'react';
+import {View} from 'react-native';
+
+import {styles} from './styles';
+
+export function Widget(){
+    return(
+        //Vale destacar que a View é como se fosse uma Div na Web
+        <View style={styles.container}>
+          
+        </View>
+    )
+}
